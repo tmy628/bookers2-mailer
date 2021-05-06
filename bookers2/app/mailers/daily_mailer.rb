99f27c5a3f4a010608b6d11d7d7fdd55752ba8d1
tmy628 <tmy6h1r@gmail.com>
2021-05-06 23:27:30 +0000

    [edit] DailyMailerの編集

--- bookers2/app/mailers/daily_mailer.rb
+++ bookers2/app/mailers/daily_mailer.rb
@@ -6,8 +6,8 @@ class DailyMailer < ApplicationMailer
   #   en.daily_mailer.daily_notification.subject
   #
   def daily_notification
-    @greeting = "Hi"
+    default to: -> { User.plick(:email) }
 
-    mail to: "to@example.org"
+    mail(subject: "Daily Report of Your Record")
   end
 end
